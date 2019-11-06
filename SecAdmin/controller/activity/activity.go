1038e6bbf2465159bd820af7a1838688787a60ab
张麟 <zlinxx@foxmail.com>
2019-11-06 19:50:09 +0800

    CreateActivity contoller add

--- SecAdmin/controller/activity/activity.go
+++ SecAdmin/controller/activity/activity.go
@@ -2,6 +2,7 @@ package activity
 
 import (
 	"SecondKill/SecAdmin/model"
+	"fmt"
 	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/logs"
 )
@@ -28,4 +29,65 @@ func (p *ActivityController) ListActivity() {
 	p.TplName = "activity/list.html"
 	p.Layout = "layout/layout.html"
 	return
+}
+
+func (p *ActivityController) SubmitActivity() {
+	activityModel := model.NewActivityModel()
+	var activity model.Activity
+
+	p.TplName = "activity/list.html"
+	p.Layout = "layout/layout.html"
+
+	var err error
+	var Error string = "success"
+	defer func() {
+		if err != nil {
+			p.Data["Error"] = Error
+			p.TplName = "activity/error.html"
+		}
+	}()
+
+	name := p.GetString("activity_name")
+	if len(name) == 0 {
+		Error = "活动的名字不能为空"
+		err = fmt.Errorf("activity name can not be null")
+		return
+	}
+
+	productId, err := p.GetInt("product_id")
+	if err != nil {
+		err = fmt.Errorf("商品id非法, err:%v", err)
+		Error = err.Error()
+		return
+	}
+
+	startTime, err := p.GetInt64("start_time")
+	if err != nil {
+		err = fmt.Errorf("开始时间非法, err:%v", err)
+		Error = err.Error()
+		return
+	}
+
+	endTime, err := p.GetInt64("end_time")
+	if err != nil {
+		err = fmt.Errorf("结束时间非法, err:%v", err)
+		Error = err.Error()
+		return
+	}
+
+	total, err :=p.GetInt("Total")
+	if err != nil {
+		err = fmt.Errorf("商品数量非法, err:%v", err)
+		Error = err.Error()
+		return
+	}
+
+	activity.ActivityName = name
+	activity.ProductId = productId
+	activity.StartTime = startTime
+	activity.EndTime = endTime
+	activity.Total = total
+
+	err = activityModel
+
 }
--- SecAdmin/model/activity.go
+++ SecAdmin/model/activity.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"github.com/astaxie/beego/logs"
 	"time"
 )
@@ -12,13 +13,13 @@ const (
 )
 
 type Activity struct {
-	ActivityId   int   `db:"id"`
-	ActivityName int   `db:"name"`
-	ProductId    int   `db:"product_id"`
-	StartTime    int64 `db:"start_time"`
-	EndTime      int64 `db:"end_time"`
-	Total        int   `db:"total"`
-	Status       int   `db:"status"`
+	ActivityId   int    `db:"id"`
+	ActivityName string `db:"name"`
+	ProductId    int    `db:"product_id"`
+	StartTime    int64  `db:"start_time"`
+	EndTime      int64  `db:"end_time"`
+	Total        int    `db:"total"`
+	Status       int    `db:"status"`
 
 	StartTimeStr string
 	EndTimeStr   string
@@ -59,5 +60,70 @@ func (p *ActivityModel) GetActivityList() (activityList []*Activity, err error) 
 		}
 	}
 	logs.Debug("get activity success, activityList is [%v]", activityList)
+	return
+}
+
+func (p *ActivityModel) CreateActivity(activity *Activity) (err error) {
+	valid, err := p.ProductValid(activity.ProductId, activity.Total)
+	if err != nil {
+		logs.Error("product exists falied, err:%v", err)
+		return
+	}
+	if !valid {
+		err = fmt.Errorf("product_id [%v], err:%v", activity.ProductId, err)
+		logs.Error(err)
+		return
+	}
+
+	if activity.StartTime <= 0 || activity.EndTime <= 0 {
+		err = fmt.Errorf("invalid start_time[%v]|end_time[%v]", activity.StartTime, activity.EndTime)
+		logs.Error(err)
+		return
+	}
+
+	if activity.EndTime <= activity.StartTime {
+		err = fmt.Errorf("start_time[%v] is greater than end_time[%v]", activity.StartTime, activity.EndTime)
+		logs.Error(err)
+		return
+	}
+
+	now := time.Now().Unix()
+	if activity.EndTime <= now || activity.StartTime <= now {
+		err = fmt.Errorf("start_time[%v]|end_time[%v] is less than now[%v]", activity.StartTime, activity.EndTime,
+			now)
+		logs.Error(err)
+		return
+	}
+
+	sqlStr := "insert into activity(name,product_id,start_time,end_time,total values(?,?,?,?,?)"
+	_, err = Db.Exec(sqlStr, activity.ActivityName, activity.ProductId, activity.StartTime, activity.EndTime,
+		activity.Total)
+	if err != nil {
+		logs.Warn("insert into activity failed, err:%v", err)
+		return
+	}
+	logs.Debug("insert into database succ")
+
+	return
+}
+
+func (p *ActivityModel) ProductValid(productId, total int) (valid bool, err error) {
+	sqlStr := "select id,name,total,status from product where id=?"
+	var productList []*Product
+	err = Db.Select(&productList, sqlStr, productId)
+	if err != nil {
+		logs.Warn("select product failed, err:%v", err)
+		return
+	}
+
+	if len(productList) == 0 {
+		err = fmt.Errorf("product[%v] is not existed", productId)
+		return
+	}
+	if total > productList[0].Total {
+		err = fmt.Errorf("product[%v]的数量非法", productId)
+		return
+	}
+	valid = true
 	return
 }
